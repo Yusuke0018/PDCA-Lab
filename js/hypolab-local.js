034becd2a8253af5d4e366e9ae98116b7d13be43
総合クリニック大阪1診 <osakasoshin1@sougoukurinikkuoosaka1shinnoiMac.local>
2025-08-20 11:31:24 +0900

    キャッシュクリアのためバージョンタグを更新

--- js/hypolab-local.js
+++ js/hypolab-local.js
@@ -1,7 +1,7 @@
         // PWA: service worker 登録
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
-                const SW_VERSION_TAG = '20250119-04';
+                const SW_VERSION_TAG = '20250120-01';
                 const SW_FILE = `./sw.v20250119-03.js?v=${SW_VERSION_TAG}`; // 新ファイル名で確実に更新
                 navigator.serviceWorker.register(SW_FILE)
                     .then(reg => {
